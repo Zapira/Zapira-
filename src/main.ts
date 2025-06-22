c4b0b01eb338b4720ab3642e2bf0b266e143fa47
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-06-22 17:42:23 +0800

    feat:setup ws and venom

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
+import { initVenom } from './whatsapp/venom';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  await initVenom(
+    (qr) => console.log('QR code ready'),
+    () => console.log('WhatsApp ready!'),
+    () => console.log('WhatsApp disconnected.')
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+
+  
 }
 bootstrap();
